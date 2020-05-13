edc7c5d8243dc4091ee4f273740ec1576cc51714
Jon Selzer <jselzer626@gmail.com>
2020-05-13 15:29:40 -0400

    add login required decorator

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url="users/")
 def index(request):
     return render(request, "orders/menu.html")

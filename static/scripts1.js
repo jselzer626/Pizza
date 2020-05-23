6f922d3b9ad13bcaa0a2fc3fa47a1a15c017633e
Jon Selzer <jselzer626@gmail.com>
2020-05-22 22:07:04 -0400

    changed menu display

--- static/scripts1.js
+++ static/scripts1.js
@@ -3,14 +3,23 @@
 document.addEventListener("DOMContentLoaded", () => {
     let menuHeader = document.querySelector('.menuHeader')
 
+    //select pasta
+    document.querySelector('#Pasta').classList.add('selected')
+    menuHeader.querySelector('p[data-section="Pasta"]').style.fontWeight = 'bold'
 
     // ----------------------------------------------------------------------------------------------------------------------------
     // configure buttons
     menuHeader.querySelectorAll('p').forEach(button => {
       button.addEventListener("click", e => {
         // section names are stored as data attributes in each header label
+        //remove previous bolding and selected section
+        var previous = document.querySelector('.selected')
+        menuHeader.querySelector(`p[data-section="${previous.id}"]`).style.fontWeight = ''
+        previous.classList.remove('selected')
+
+        //add bolding and visibility to current section
         var sectionToDisplay = document.getElementById(`${e.target.dataset.section}`)
-        console.log(sectionToDisplay)
+        e.target.style.fontWeight = 'bold'
         sectionToDisplay.classList.add('selected')
       })
     })

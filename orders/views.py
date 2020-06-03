16a95e70fd30d1d21dcae622ccead3df1fa88dc7
Jon Selzer <jselzer626@gmail.com>
2020-06-03 16:14:45 -0400

    fixed non logged in nav bar url paths

--- orders/views.py
+++ orders/views.py
@@ -62,8 +62,10 @@ class editItem(UpdateView):
     template_name = "orders/editItem.html"
     success_url = reverse_lazy("viewCart")
 
-    def get_initial(self):
-        print(self.kwargs['pk'])
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        print(context['object'].item)
+        return context
 
 class addGeneralItem(CreateView):
     model = OrderDetail

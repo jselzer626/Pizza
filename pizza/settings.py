ee8789ed15f8a9f5404c96158543099c9fab36a2
Jon Selzer <jselzer626@gmail.com>
2020-07-02 14:35:02 -0400

    changed host again

--- pizza/settings.py
+++ pizza/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["jls-pizza.herokuapp.com/"]
+ALLOWED_HOSTS = ["jls-pizza.herokuapp.com"]
 
 # Application definition
 
